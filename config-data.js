285079b19d67f5b7924d8640e0886aaf1d9e0a5b
Kev <kev22.cadette@hotmail.com>
2021-01-26 20:16:26 +0000

    Removeslashes

--- config-data.js
+++ config-data.js
@@ -1,12 +1,12 @@
 module.exports.replace = [
-  'define( \'AUTH_KEY\',         \'put your unique phrase here\' )\;',
-  'define( \'SECURE_AUTH_KEY\',  \'put your unique phrase here\' )\;',
-  'define( \'LOGGED_IN_KEY\',    \'put your unique phrase here\' )\;',
-  'define( \'NONCE_KEY\',        \'put your unique phrase here\' )\;',
-  'define( \'AUTH_SALT\',        \'put your unique phrase here\' )\;',
-  'define( \'SECURE_AUTH_SALT\', \'put your unique phrase here\' )\;',
-  'define( \'LOGGED_IN_SALT\',   \'put your unique phrase here\' )\;',
-  'define( \'NONCE_SALT\',       \'put your unique phrase here\' )\;'
+  'define( \'AUTH_KEY\',         \'put your unique phrase here\' );',
+  'define( \'SECURE_AUTH_KEY\',  \'put your unique phrase here\' );',
+  'define( \'LOGGED_IN_KEY\',    \'put your unique phrase here\' );',
+  'define( \'NONCE_KEY\',        \'put your unique phrase here\' );',
+  'define( \'AUTH_SALT\',        \'put your unique phrase here\' );',
+  'define( \'SECURE_AUTH_SALT\', \'put your unique phrase here\' );',
+  'define( \'LOGGED_IN_SALT\',   \'put your unique phrase here\' );',
+  'define( \'NONCE_SALT\',       \'put your unique phrase here\' );'
 ];
 
 module.exports.standard = [
